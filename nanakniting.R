4e11170f312ea3797ccc6d6c61054cc381e1e166
Matt Cooper <mattcstats@gmail.com>
2012-11-06 21:04:45 +0800

    fifth crack

--- nanakniting.R
+++ nanakniting.R
@@ -1,6 +1,13 @@
+# github code
+
+
 library(knitr)
 setwd("/Users/mcooper/Documents/git/blog_code")
 knit("petrol_prices.Rmd")
 system("pandoc -s petrol_prices.md -o petrol_prices.html")
 knit("ggplot_post_text_example.Rmd")
 system("pandoc -s ggplot_post_text_example.md -o ggplot_post_text_example.html")
+knit("nfl_team_picker_example.Rmd")
+system("pandoc -s nfl_team_picker_example.md -o nfl_team_picker_example.html")
+knit("forest_plot.Rmd")
+system("pandoc -s forest_plot.md -o forest_plot.html")
