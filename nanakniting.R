4cbe2da1702ec3574c7ecb8ad3b7911fff7b60c9
nzcoops <mattcstats@gmail.com>
2014-06-02 14:05:00 +0800

    Update nanakniting.R

--- nanakniting.R
+++ nanakniting.R
@@ -1,8 +1,15 @@
 # github code
-
+# copy into blog folder, 'add', put in a comment, then push
+# pop-mac-a1138:blog_code mcooper$ cp forest_plot.Rmd blog_code/
+# cp nanakniting.R blog_code/
+# cd blog_code/
+# git add forest_plot.Rmd 
+# git add nanakniting.R 
+# git commit -m 'fifth crack'
+# git push https://github.com/nzcoops/blog_code
 
 library(knitr)
-setwd("/Users/mcooper/Documents/git/blog_code")
+setwd("/Users/mcooper/Documents/documents_random/git/blog_code")
 knit("petrol_prices.Rmd")
 system("pandoc -s petrol_prices.md -o petrol_prices.html")
 knit("ggplot_post_text_example.Rmd")
@@ -11,3 +18,5 @@ knit("nfl_team_picker_example.Rmd")
 system("pandoc -s nfl_team_picker_example.md -o nfl_team_picker_example.html")
 knit("forest_plot.Rmd")
 system("pandoc -s forest_plot.md -o forest_plot.html")
+knit("shading_between_the_lines.Rmd")
+system("pandoc -s shading_between_the_lines.md -o shading_between_the_lines.html")
